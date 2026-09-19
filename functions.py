00ec2841a76f006f9c7815f0d7ad5a7d7da1b6c9
abhay <abhaykumar77549@gmail.com>
2026-09-20 00:02:36 +0530

    v

--- functions.py
+++ functions.py
@@ -31,3 +31,14 @@ def factorial(n):
     return(factorial)
 
 print(factorial(5))   #example
+
+
+'function count_digits(n) that returns the number of digits.'
+def count_digit(n):
+    count = 0
+    while n > 0:
+       n = n//10
+       count += 1
+    return(count) 
+
+print(count_digit(123345676))
