a541f6b2eff5d35510fbb76e110ac00af9aef37b
abhay <abhaykumar77549@gmail.com>
2026-09-19 23:08:15 +0530

    Find Second Largest

--- functions.py
+++ functions.py
@@ -3,4 +3,12 @@ def simple_intrest(p, r, t):
     SI = (p * r * t)/100
     return SI
 
-print(simple_intrest(10, 20, 30))
+print(simple_intrest(10, 20, 30))  #example
+
+
+'Find Second Largest'
+def largest_number(a, b, c, d):
+    LN = max(a, b, c, d)
+    return LN
+
+print(largest_number(1, 2, 3, 4))   #example
